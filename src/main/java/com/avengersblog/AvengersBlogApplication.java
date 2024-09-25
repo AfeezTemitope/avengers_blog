5f2eecc6409f7cf71bc0316e779259e9071e808e
Hokage <oyebolatomilade@gmail.com>
2024-09-25 13:47:07 +0100

    First commit

---
+++ src/main/java/com/avengersblog/AvengersBlogApplication.java
@@ -0,0 +1,13 @@
+package com.avengersblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvengersBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avengersblog/AvengersBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avengersblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
