58247917c378cff2e8def4c841a61187810514d1
unknown <belloafeez28@gmail.com>
2024-09-27 20:44:19 +0100

    add

--- src/main/java/com/avengersblog/Data/Repository/PostRepository.java
+++ src/main/java/com/avengersblog/Data/Repository/PostRepository.java
@@ -3,6 +3,6 @@ package com.avengersblog.Data.Repository;
 import com.avengersblog.Data.Model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Integer> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
